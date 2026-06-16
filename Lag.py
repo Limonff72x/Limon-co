cb8e5c427b9e4a7d9f2bf5d5b20885451d342000
Limonff72x <limonshots1234@gmail.com>
2026-06-17 05:37:48 +0600

    Create Lag.py

---
+++ Lag.py
@@ -0,0 +1,8 @@
+{
+  "showUpdate": false,
+  "title": "🔔 New Update Available!",
+  "message": "Please update the app to continue using it.",
+  "buttonText": "Update Now",
+  "updateUrl": "https://t.me/+k1IpwIj6muBmNDFl",
+  "credit": "LIMON. X 72X"
+}
